7382d00cc8e4ccd4c31d80f58847f64a107c3456
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-07-20 12:55:33 -0500

    .env added, MySQL DB added

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'tasks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': environ.get('DB_NAME'),
+        'USER': environ.get('DB_USER'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'PORT': environ.get('DB_PORT'),
+        'HOST': environ.get('DB_HOST'),
     }
 }
 

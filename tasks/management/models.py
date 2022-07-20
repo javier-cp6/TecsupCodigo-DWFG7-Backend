292d93221c1ce5762e8b9f03b6b7fed437aa7cfe
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-07-20 00:14:50 -0500

    Task model added

--- tasks/management/models.py
+++ tasks/management/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+  PENDING = 'PE'
+  IN_PROGRESS = 'IP'
+  DONE = 'DO'
+  statusOptions = [(PENDING, 'Pending'), (IN_PROGRESS, 'In progress'), (DONE,'Done')]
+
+  id = models.AutoField(primary_key=True, unique=True)
+  name = models.CharField(db_column='task_name', max_length=100)
+  description = models.TextField(db_column='task_desc', null=True)
+  deadline = models.DateField(db_column='task_deadline')
+  status = models.CharField(db_column='task_status', choices = statusOptions, max_length=2, default=PENDING)
+
+  class Meta:
+    db_table = 'tasks'
+    ordering = ['-deadline']

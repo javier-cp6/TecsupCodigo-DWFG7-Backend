3bde0a842904721b8e7a4af5e34e8d199551fc5b
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-07-20 18:44:33 -0500

    management.views.py added

--- tasks/management/views.py
+++ tasks/management/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+@api_view(http_method_names=['GET', 'POST'])
+def start(request: Request):
+  print(request)
+  print(request.data) 
+  return Response(data={
+    'message': 'Decorator endpoint'
+  })
--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'management'
+    'management',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [

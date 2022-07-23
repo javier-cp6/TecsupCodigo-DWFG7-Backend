fef7be26ea03dc865b2374b4c1943f4e64d78b6e
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-07-22 19:12:59 -0500

    RegisterView created

--- tasks/authentication/urls.py
+++ tasks/authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from .views import RegisterView
+
 urlpatterns = [
-    path('login', TokenObtainPairView.as_view())
+    path('login', TokenObtainPairView.as_view()),
+    path('register', RegisterView.as_view())
 ]
 
 
--- tasks/authentication/views.py
+++ tasks/authentication/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.request import Request
+from rest_framework.generics import CreateAPIView
+from .serializers import RegisterSerializer
+from .models import User
+
+class RegisterView(CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = RegisterSerializer
+
+  # def post(self, request: Request):
+  #   body = request.data
+  #   serializerInstance = self.serializer_class(data=body)
+
+  #   validation = serializerInstance.is_valid(raise_exception=True)
+  #   if validation == True:
+  #     serializerInstance.save()
+
+

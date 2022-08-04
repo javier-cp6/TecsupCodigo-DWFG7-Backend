258c9d2e3b77c93087d6bb005478cb1a6a5b08ca
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-08-04 13:23:28 -0500

    deploy en heroku

--- src/server.js
+++ src/server.js
@@ -1,10 +1,46 @@
 import express from 'express'
 // import { PrismaConnector } from './prisma.js';
+import cors from "cors";
 import { departamentosRouter } from './routes/departamentos.routes.js'
 import { trabajadoresRouter } from './routes/trabajadores.routes.js'
 
 const app = express()
 const PORT = process.env.PORT
+
+// el método GET siempre podrá ser accedido ya que es utilizado por el navegador
+
+// método 1: Configuring CORS
+// declarar origin, methods y headers permitidos
+// cors() puede recibir un json de opciones (método 1) o una función (método 2)
+app.use(
+  cors({
+    origin: ["https://www.tecsup.edu.pe"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["accept", "authorization", "content-type"]
+  })
+)
+
+// método 2: Configuring CORS w/ Dynamic Origin
+// usa callback
+
+// lista de orígnes permitidos, es decir dominios que  pueden consultar API
+/*
+const origenesPermitidos = ["http://miaplicacion.com"]; // lista blanca
+
+app.use(
+  cors((req, cb) => {
+    const corsOptions = { origin: false};
+
+    if (origenesPermitidos.indexOf(req.header("Origin") ?? "") !== -1) {
+      corsOptions.origin = true
+    }
+
+    cb(null, corsOptions)
+  })
+)
+*/
+
+
 /*
 app.get('/departamentos', async (req, res) => {
   try {

a1186281c73644e40a1bd74a93c9157561f9c1b8
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-09-09 18:10:05 -0500

    SECRET_KEY set up

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3t5xbe-&as^idil@#rchp+%mc#ko3qg#&1t&27wl%s71!j#!bq'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
